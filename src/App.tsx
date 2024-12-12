510fa8799b76d4e08bc81a28e57bc9c7ae29952d
Sylpherena <rbb.burcu@gmail.com>
2024-12-12 15:02:03 +0300

    theme provider created

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Applications from "./pages/Applications/Applications";
 import Navigation from "./Navigation";
-import { DaisyUIThemes } from "./themes";
-import { useEffect, useState } from "react";
+import ThemeProvider from "./providers/theme/ThemeProvider";
 
 function App() {
-  const [themeMode, setThemeMode] = useState<DaisyUIThemes>(() => {
-    return (localStorage.getItem("theme") || "light") as DaisyUIThemes;
-  });
-
-  useEffect(() => {
-    // Apply the current theme to the document's root element
-    document.documentElement.setAttribute("data-theme", themeMode);
-    // Save the current theme to local storage
-    localStorage.setItem("theme", themeMode);
-  }, [themeMode]);
-
-  const changeTheme = (theme: DaisyUIThemes) => {
-    setThemeMode(theme);
-  };
-
   return (
-    <>
-      <Navigation onChangeTheme={changeTheme} theme={themeMode} />
+    <ThemeProvider>
+      <Navigation />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/applications" element={<Applications />} />
         </Routes>
       </Router>
-    </>
+    </ThemeProvider>
   );
 }
 
